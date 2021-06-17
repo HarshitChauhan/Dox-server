7e3b3ba9516e7d3f57a9885e24b23efb60189d4f
HarshitChauhan <harshitkumar2503@gmail.com>
2021-06-17 18:27:55 +0530

    socket io setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const io=require("socket.io")(3001,{
-    cors:{
-        origin:'http://localhost:3000',
-        methods:["GET", "POST"],
-    }
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
+io.on("connection", (socket) => {
+  console.log("Connected...");
+});
