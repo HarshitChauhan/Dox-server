682187fc450fb0877d87ce4fce4da36c85738df4
HarshitChauhan <harshitkumar2503@gmail.com>
2021-06-18 00:38:56 +0530

    made separate room for each doc

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const io = require("socket.io")(3001, {
 });
 
 io.on("connection", (socket) => {
-  //send-changes event
-  socket.on("send-changes", delta => {
-    // broadcasting changes to everyone else that are recieved from client
-    socket.broadcast.emit("recieve-changes",delta);
-  })
+
+  //get-document event if data is present in document
+  socket.on("get-document", documentId=>{
+    const initialData="";
+    // it joins a seperate room for every document
+    socket.join(documentId);
+    socket.emit("load-document", initialData);
+    
+    //send-changes event
+    socket.on("send-changes", delta => {
+      // broadcasting changes to everyone else that are recieved from client
+      socket.broadcast.to(documentId).emit("recieve-changes",delta);
+    });
+  });
+  //console.log(documentId);
+
   
 });
