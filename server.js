73f32495187d19e2709053d927e5a55ea9b48da2
HarshitChauhan <harshitkumar2503@gmail.com>
2021-06-17 18:59:45 +0530

    broadcasting changes

--- server.js
+++ server.js
@@ -6,5 +6,10 @@ const io = require("socket.io")(3001, {
 });
 
 io.on("connection", (socket) => {
-  console.log("Connected...");
+  //send-changes event
+  socket.on("send-changes", delta => {
+    // broadcasting changes to everyone else that are recieved from client
+    socket.broadcast.emit("recieve-changes",delta);
+  })
+  
 });
