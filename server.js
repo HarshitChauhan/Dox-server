4441168a2c1a6a33a9f5ba926d0a9c4f2f00b01c
Harshit Kumar <harshit.a.kumar@apisero.com>
2023-07-01 19:21:29 +0530

    migrated from github

---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const Document = require("./Document");
+const PORT = process.env.PORT || 3001;
+const connectionString = process.env.DATABASE_URL;
+
+console.log("Starting Dox Server...");
+// console.log(`${process.env.DATABASE_URL}\n and ${process.env.FRONTEND_URL}\n and ${process.env.PORT}`);
+
+const result =  mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}).then(() =>{
+  console.log('Database Connection Successfull!');
+});
+
+//connection with client
+const io = require("socket.io")(PORT, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  //get-document event if data is present in document
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    
+    // it joins a seperate room for every document
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+    
+    //send-changes event
+    socket.on("send-changes", (delta) => {
+      // broadcasting changes to everyone else that are recieved from client
+      socket.broadcast.to(documentId).emit("recieve-changes", delta);
+    });
+
+    // saving our content
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+  
+  const document = await Document.findById(id);
+  //if present return it
+  if (document) return document;
+  // else create new return it
+  return await Document.create({ _id: id, data: defaultValue });
+}
+
+console.log(`Dox Server has been started successfully on Port: ${process.env.PORT}!`);
